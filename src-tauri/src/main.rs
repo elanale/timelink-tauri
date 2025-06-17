ab8f0bc6635d5af6394ef2a4b3c1cd14ef7c902b
Elan Wygodski <166648947+elanale@users.noreply.github.com>
2025-06-17 18:43:50 -0400

    wip

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 fn main() {
-    timelink_lib::run()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
